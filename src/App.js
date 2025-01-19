9f84be8ac4a1ff2d31c209ba478d4c75ab0782a9
VasanthBalasubramaniyan <vambuvasanth124@gmail.com>
2025-01-19 13:10:04 +0530

    Made some changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Chat from "./components/Chat"
+import Search from "./components/Search"
+
+function App() {
+    return (
+        <div>
+            <Search></Search>
+            <Chat></Chat>
+        </div>
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import Search from "./components/Search";
-import Chat from "./components/Chat"
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <Search></Search>
-    <Chat></Chat>
+    <App></App>
   </div>
 );
