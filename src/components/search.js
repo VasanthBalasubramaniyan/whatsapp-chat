6c24996a0ba23af1290efb13672e8a6ef959b922
VasanthBalasubramaniyan <vambuvasanth124@gmail.com>
2025-01-19 11:52:12 +0530

    Created Files

---
+++ src/components/search.js
@@ -0,0 +1,11 @@
+// Search Component
+
+function Search() {
+    return(
+        <div>
+            <input type = "text"></input>
+        </div>
+    )
+}
+
+export default Search
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Search from './components/search';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Search></Search>);
+
